70ea3c99d8e2040df2da6d62f93268fae500003d
weihaibo <weihaibo@bogon>
2019-05-05 12:33:15 +0800

    update cors filter

--- api/src/main/java/com/simon/cloud/leaf/api/framework/filter/CustomCorsFilter.java
+++ api/src/main/java/com/simon/cloud/leaf/api/framework/filter/CustomCorsFilter.java
@@ -1,10 +1,8 @@
 package com.simon.cloud.leaf.api.framework.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,16 +15,31 @@ import java.io.IOException;
  * Aug 3, 2016
  */
 @Slf4j
-public class CustomCorsFilter extends OncePerRequestFilter {
+public class CustomCorsFilter implements Filter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+
         //设置过滤器内返回的返回值类型
         httpServletResponse.setCharacterEncoding("UTF-8");
         httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");      //为安全起见，可配置允许访问的请求方地址。这里配置成*号，是允许所有访问。
         httpServletResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");        //为安全起见，也可配置成只允许POST请求
         httpServletResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, token");      //这里要注意，token是自定义的请求头当中带的token，在这里必须添加，否则你永远获取不到。
         httpServletResponse.addHeader("Access-Control-Max-Age", "1800");//30 min
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        chain.doFilter(httpServletRequest, httpServletResponse);
     }
+
+    @Override
+    public void destroy() {
+
+    }
+
 }
